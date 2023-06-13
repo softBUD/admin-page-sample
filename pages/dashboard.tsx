49ca010682c06f2fc3b1d7a4cef82fc8dc6a3819
softBUD <chevimpala21@gmail.com>
2023-06-13 14:26:47 +0900

    card components 만드는중

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Card from "@/components/card";
 
-function dashboard() {
-  return <div>dashboard</div>;
+export default function dashboard() {
+  return (
+    <div className="bg-slate-100 w-screen h-screen">
+      <div className="max-w-xl p-4">
+        <Card title="타이틀" content="콘텐트" />
+      </div>
+    </div>
+  );
 }
-
-export default dashboard;
