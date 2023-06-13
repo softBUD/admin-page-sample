715a3b0ba3d31910d14e6b6be4ad35d22e70b81d
imjungim <jungim4201@gmail.com>
2023-06-13 10:19:44 +0900

    header markup

---
+++ components/header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav>
+      <h2>menu</h2>
+      <ul>
+        <li>
+          <a href="/">Dashboard</a>
+        </li>
+        <li>
+          <a href="/">Users</a>
+        </li>
+        <li>
+          <a href="/">Settings</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
