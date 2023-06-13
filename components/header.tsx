98fbd2ccde2e20ab79ba354b131a0539b96e294f
imjungim <jungim4201@gmail.com>
2023-06-13 16:51:30 +0900

    header icon 추가

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { FaMailBulk } from "react-icons/fa";
+import { FaBell } from "react-icons/fa";
 
 export default function Header() {
   return (
@@ -18,18 +21,39 @@ export default function Header() {
         </ul>
       </nav>
 
-      <div className="border border-indigo-200 rounded-md">
-        <input className="pl-2" type="text" placeholder="Search"/>
-        <button>
-          
-        </button>
-      </div>
+      <div className="flex items-center">
+        <div className="border">
+          <div>
+            <button className="">
+              <FaSearch className="" />
+            </button>
+            <input className="pl-2 truncate" type="text" placeholder="Search" />
+          </div>
+        </div>
 
-      {/* 로그인 했을 경우 */}
-      <div>
+        <div className="flex">
+          <div className="flex items-center">
+            <FaMailBulk className="mr-4 text-xl	" />
+            <FaBell className="mr-4 text-xl	" />
+          </div>
 
+          {/* 로그인 했을 경우 */}
+          <div className="w-full ">
+            <button
+              className="w-8 h-8 rounded-full overflow-hidden"
+              type="button"
+            >
+              <img
+                className="block w-full h-full object-cover"
+                src="./images.jpg"
+                alt="프로필 이미지"
+              />
+            </button>
+          </div>
+          {/* 로그인 하지 않은 경우 */}
+          <div></div>
+        </div>
       </div>
-      {/* 로그인 하지 않은 경우 */}
     </header>
   );
 }
